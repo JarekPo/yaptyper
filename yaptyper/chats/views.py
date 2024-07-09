39782a0a47e6d10278b05e5c6342c9d01a39d975
Jaroslaw Polowy <jarekpolowy@gmail.com>
2024-07-09 08:54:48 +0100

    update tailwind settings, folders structure and paths

--- yaptyper/chats/views.py
+++ yaptyper/chats/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, "../templates/chats/index.html")
+def index(request, username):
+    return render(request, "chats/index.html", {"username": username})
 
 
 def room(request, room_name):
-    return render(request, "../templates/chats/room.html", {"room_name": room_name})
+    return render(request, "chats/room.html", {"room_name": room_name})
