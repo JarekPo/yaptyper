bcea07c4ef0d8c2c3a162c93cf70bdae8dd7584b
Jaroslaw Polowy <jarekpolowy@gmail.com>
2024-07-19 08:41:36 +0100

    update settings

--- yaptyper/build_files.sh
+++ yaptyper/build_files.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+VIRTUAL_ENV=venv
+
+if [ -d "$VIRTUAL_ENV" ]; then
+  source $VIRTUAL_ENV/bin/activate
+fi
+
 $(which pip) install -r requirements.txt
 
 $(which python) manage.py collectstatic --noinput
