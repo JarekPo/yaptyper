c186d2ff4e916753ddb769028a90807d151a5268
Jarek Polowy <jarekpolowy@gmail.com>
2024-12-21 20:10:01 +0000

    Y-49: detach socketio logic from manage.py

---
+++ yaptyper/yaptyper/socketio_handlers/utils.py
@@ -0,0 +1,18 @@
+import random
+
+def generate_random_color():
+    chat_text_colors = [
+        "#003366",
+        "#990000",
+        "#5B2E91",
+        "#0033CC",
+        "#4B3D28",
+        "#007A7A",
+        "#CC6600",
+        "#FF1493",
+        "#6B8E23",
+        "#2F4F4F",
+        "#4B0082",
+        "#A52A2A",
+    ]
+    return random.choice(chat_text_colors)
